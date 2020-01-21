8f303f013193cb5a09c4263562839e92c8915ccc
yuancao <sam.cao918@gmail.com>
2020-01-20 22:23:59 -0500

    added comments

--- Pow(x,n)再看.py
+++ Pow(x,n)再看.py
@@ -8,6 +8,9 @@ class Solution:
     def myPow(self, x, n):
         # 接着刷
         # Take care of the case when n < 0
+        # 磨2除2是为了转换2进制，if判定如果当前位置为一时ans乘x，反之不乘。
+        # Example 3^6 = 3^4 * 3^2 = 1*3^4 + 1*3^2 + 0*3^1:
+        # 磨第一此时个位为0，不乘，不过底数要自己乘自己(3^2)，因为已经到了十位，是2次蜜了
         if n < 0:
             x = 1 / x
             n = -n
@@ -17,9 +20,7 @@ class Solution:
         while n > 0:
             if n % 2 == 1:
                 ans = ans * current_product
-                n = n - 1
-            else:
                 # when the power is even, double the current product, (e.g. 2^4 = (2^2)^2)
-                current_product = current_product * current_product
-                n = n // 2
+            current_product = current_product * current_product
+            n = n // 2
         return ans
